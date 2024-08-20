64993f7925d02815915881dfe101d4720180ee8c
samer.mazraani <samer.mazraani96@gmail.com>
2024-08-20 21:07:32 +0300

    finalizing functionilities of darkmode and side bar collapsed with redux with  styling

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,31 +1,48 @@
-"use client"
-import Navbar from '@/app/components/Navbar'
-import Sidebar from './components/Sidebar'
-import StoreProvider, { useAppSelector } from './redux'
+"use client";
+import Navbar from "@/app/components/Navbar";
+import Sidebar from "./components/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+import { useEffect } from "react";
 
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-const DashboardLayout= ({children}: {children:React.ReactNode}) => {
-  const isSideBarCollapsed = useAppSelector((state)=> state.global.isSidebarCollapsed)
+  useEffect(() => {
+    if (!isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  }, []);
 
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      <Sidebar/>
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSideBarCollapsed? "md:pl-24" : "md:pl-72" } `}> 
-        <Navbar/>
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSideBarCollapsed ? "md:pl-24" : "md:pl-72"
+        } `}
+      >
+        <Navbar />
         {children}
       </main>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-const DashboardWrapper = ({children}: {children:React.ReactNode}) => {
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-   <StoreProvider>
-    <DashboardLayout>
-      {children}
-      </DashboardLayout>
-   </StoreProvider>
-  )
-}
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
 
-export default DashboardWrapper
+export default DashboardWrapper;
